a0ed6938ed24e6cb8d6e999fe3c86e04f1418298
Hunter Stanton <hunter_Stanton@outlook.com>
2019-09-02 03:26:32 -0500

    Blank project

---
+++ sh4texteditor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sh4texteditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
